8c7de5af0644460dc7664e376101484b1e56c081
v.svirskiy <barabashka25219@icloud.com>
2023-11-24 22:11:41 +0300

    New resource: "new_entry/<int:topic_id>" 1. Create url 2. Create template 3. Create view

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, 'learning_logs/index.html')
@@ -27,3 +27,19 @@ def new_topic(request):
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    return render(request, 'learning_logs/new_entry.html', context={'topic': topic, 'form': form})
