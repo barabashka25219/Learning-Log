0488e4a2f8280c9110e1b9e2aa57034fe30ddb04
v.svirskiy <barabashka25219@icloud.com>
2023-11-29 23:07:42 +0300

    Configure media path settings

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('learning_logs.urls')),
+    static(MEDIA_URL, document_root=MEDIA_ROOT),
 ]
