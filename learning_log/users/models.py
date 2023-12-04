c90c993c39f7418d02529fb3a55f9ff3d2e707a9
v.svirskiy <barabashka25219@icloud.com>
2023-12-04 18:58:07 +0300

    Create form for Userinfo model and render it

--- learning_log/users/models.py
+++ learning_log/users/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Userinfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(unique=True)
-    firstname = models.CharField(max_length=128)
-    lastname = models.CharField(max_length=128)
+    email = models.EmailField(unique=True, blank=True)
+    firstname = models.CharField(max_length=128, blank=True)
+    lastname = models.CharField(max_length=128, blank=True)
     
     class Meta:
         verbose_name_plural = 'userinfos'
+        
+    @receiver(post_save, sender=User)
+    def create_info(sender, instance, created, **kwargs):
+        if created:
+            Userinfo.objects.create(user=instance)
+            
+    @receiver(post_save, sender=User)
+    def save_info(sender, instance, created, **kwargs):
+        instance.userinfo.save()
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login 
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserInfoForm
 
 def register(request):
     if request.method != 'POST':
         form = UserCreationForm()
+        info_form = UserInfoForm()
     else:
+        print(dir(request.user))
         form = UserCreationForm(data=request.POST)
+        info_form = UserInfoForm(data=request.POST)
 
-        if form.is_valid():
+        if form.is_valid() and info_form.is_valid():
             new_user = form.save()
+            info_form.save()
             login(request, new_user)
             return redirect('learning_logs:index')
     
-    return render(request, 'users/register.html', context={'form': form})
+    return render(request, 'users/register.html', context={'form': form, 'info_form': info_form})
