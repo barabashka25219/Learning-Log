0ca9c6c4ab04f3b5d1ccb052c5dfd35b562c60b7
v.svirskiy <v.svirskiy@modulew.ru>
2023-12-04 18:08:01 +0300

    Create Userinfo (profile) model

--- learning_log/users/models.py
+++ learning_log/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Userinfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    
+    class Meta:
+        verbose_name_plural = 'userinfos'
